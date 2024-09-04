d8cf9401df886228f5ae09c382d59bf4b5a121ca
zhanghao <archer03371@163.com>
2024-09-04 23:51:07 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # PadMstResource
 
+## 利用環境変数
+
+
+| xxx                       | yyy         | zzz    |
+| ---                       | ---         | ---    |
+| hpsnk_padmst_master_dir   | 1.源数据目录 |
+| hpsnk_padmst_resource_dir | 2.          | 
+
+## 功能
+
+* 把 env1 目录下的资源文件，复制到 env2 目录下
+
+
 
 ## httpd 设置
 ```
--- rum.cmd
+++ rum.cmd
@@ -1,3 +1,24 @@
+@echo off
+
+rem // check env1
+if "%hpsnk_padmst_offical_dir%"=="" (
+echo "env1:hpsnk_padmst_offical_dir is empty!"
+) else (
+echo "env1:hpsnk_padmst_offical_dir is not empty."
+)
+
+rem // check env2
+if "%hpsnk_padmst_resource_dir%"=="" (
+echo "env2:hpsnk_padmst_resource_dir is empty!"
+) else (
+echo "env2:hpsnk_padmst_resource_dir is not empty."
+)
+
+exit 0
+
+
+
+
 
 rem //
 
