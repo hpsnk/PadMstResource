eb170c4ff544760392556f93c070a665530b74c8
zhanghao <archer03371@163.com>
2024-09-28 07:40:04 +0800

    update

--- rum.cmd
+++ rum.cmd
@@ -1,31 +1,37 @@
 @echo off
 
-rem // check env1
-if "%hpsnk_padmst_offical_dir%"=="" (
-echo "env1:hpsnk_padmst_offical_dir is empty!"
+rem // check env1 hpsnk_padmst_master_dir
+if "%hpsnk_padmst_master_dir%"=="" (
+echo "[ERROR][1]env1:hpsnk_padmst_master_dir is empty!"
+pause
+exit 1
 ) else (
-echo "env1:hpsnk_padmst_offical_dir is not empty."
+echo "env1:hpsnk_padmst_master_dir is not empty."
 )
 
 rem // check env2
 if "%hpsnk_padmst_resource_dir%"=="" (
-echo "env2:hpsnk_padmst_resource_dir is empty!"
+echo "[ERROR][2]env2:hpsnk_padmst_resource_dir is empty!"
+pause
+exit 2
 ) else (
 echo "env2:hpsnk_padmst_resource_dir is not empty."
 )
 
+rem //image
+mkdir %hpsnk_padmst_resource_dir%\images\cards_ja
+copy  %hpsnk_padmst_master_dir%\images\attrs.png          %hpsnk_padmst_resource_dir%\images\
+copy  %hpsnk_padmst_master_dir%\images\awoken.png         %hpsnk_padmst_resource_dir%\images\
+copy  %hpsnk_padmst_master_dir%\images\CARDFRAME2.png     %hpsnk_padmst_resource_dir%\images\
+copy  %hpsnk_padmst_master_dir%\images\card-frame-any.png %hpsnk_padmst_resource_dir%\images\
+copy  %hpsnk_padmst_master_dir%\images\CARDFRAMEW.png     %hpsnk_padmst_resource_dir%\images\
+copy  %hpsnk_padmst_master_dir%\images\type.png           %hpsnk_padmst_resource_dir%\images\
+copy  %hpsnk_padmst_master_dir%\images\cards_ja\*.png     %hpsnk_padmst_resource_dir%\images\cards_ja\
+
+rem //css
+copy  %hpsnk_padmst_master_dir%\style.css %hpsnk_padmst_resource_dir%\
+copy  %hpsnk_padmst_master_dir%\style-monsterimages.css %hpsnk_padmst_resource_dir%\
+
+pause
+
 exit 0
-
-
-
-
-
-rem //
-
-copy %hpsnk_padmst_offical_dir%\style-monsterimages.css .
-
-mkdir images
-
-mkdir images\cards_ja
-
-copy %hpsnk_padmst_offical_dir%\images\cards_ja\*.png .\images\cards_ja\
